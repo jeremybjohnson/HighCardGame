a8aaa747b5e79190d699d2aae60c8b8686c1683b
Jeremy Johnson <johnson.jeremy45@gmail.com>
2022-09-11 17:15:42 -0500

    Added repsonisive design changes to CSS, HTML and JavaScript

--- scripts/card_game.js
+++ scripts/card_game.js
@@ -13,19 +13,19 @@ class Deck {
     this.cards = [];
   }
   createDeck() {
-    var suits = ['C', 'S', 'H', 'D']
-    var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    let suits = ['C', 'S', 'H', 'D'];
+    let ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-    for (var i=0; i < suits.length; i++) {
-      for (var j=0; j < ranks.length; j++) {
+    for (let i=0; i < suits.length; i++) {
+      for (let j=0; j < ranks.length; j++) {
         this.cards.push(new Card(suits[i], ranks[j], values[j]))
       }
     }
   }
   shuffleDeck() {
-    var location1, location2, tmp;
-    for (var i= 0; i < 1000; i++) {
+    let location1, location2, tmp;
+    for (let i= 0; i < 1000; i++) {
       location1 = Math.floor(Math.random() * this.cards.length)
       location2 = Math.floor(Math.random() * this.cards.length)
       tmp = this.cards[location1];
@@ -35,10 +35,10 @@ class Deck {
   }
 }
 
-// set global variables for player scores and turn counter
-var player1Score = 0;
-var player2Score = 0;
-var turnCounter = 1;
+// set global let iables for player scores and turn counter
+let player1Score = 0;
+let player2Score = 0;
+let turnCounter = 1;
 
 function startGame() {
   // Reset scores and game counter
@@ -51,16 +51,16 @@ function startGame() {
   turnCounter = 1;
   // reset onclick for cards, random card backs and remove card styles
   // count over 3 rows
-  for(var i = 1; i < 4; i++) {
+  for(let i = 1; i < 4; i++) {
     // count over 4 columns
-    for(var j = 1; j < 5; j++){
+    for(let j = 1; j < 5; j++){
       // reset onclick
-      var x = document.getElementById("card" + i.toString() + j.toString())
+      let x = document.getElementById("card" + i.toString() + j.toString())
       x.disabled = false;
       // generate random number for card backs
-      var num = getRandomInt(1, 6);
+      let num = getRandomInt(1, 6);
       // random card back design
-      x.innerHTML = "<img src='PNG/backs/" + num + ".png' class='cards'>";
+      x.innerHTML = "<img src='PNG/backs/" + num + ".png' class='cards img-fluid'>";
       // reset onclick borders and background colors for cards
       x.style.border = "";
       x.style.backgroundColor = "";
@@ -102,28 +102,28 @@ function checkWinner() {
   if (player2Score > player1Score) {
     alert("Player 2 Wins " + player2Score + " to " + player1Score  + ". Press start to try again.");
   }
-  if (player2Score == player1Score) {
+  if (player2Score === player1Score) {
     alert("We have a tie. Press start to try again.");
   }
 }
 
 function countCard(cardId) {
   // generate and shuffle a deck of cards
-  var deck = new Deck();
+  let deck = new Deck();
   deck.createDeck();
   deck.shuffleDeck();
   // draw the first card - use rank and suit to form image name
-  var cardDrawn = deck.cards[0].rank + deck.cards[0].suit + ".png";
-  var cardValue = deck.cards[0].value;
+  let cardDrawn = deck.cards[0].rank + deck.cards[0].suit + ".png";
+  let cardValue = deck.cards[0].value;
 
-  var x = document.getElementById(cardId)
+  let x = document.getElementById(cardId)
   // insert image of drawn card
-  x.innerHTML = "<img src='PNG/" + cardDrawn + "' class='cards'>";
+  x.innerHTML = "<img src='PNG/" + cardDrawn + "' class='cards img-fluid'>";
   // disable onclick
   x.disabled = true;
 
   // put colored border around card based on player number
-  if (turnCounter % 2 != 0) {
+  if (turnCounter % 2 !== 0) {
     x.style.border = "3px solid IndianRed";
     x.style.borderRadius = "10px";
     x.style.backgroundColor = "IndianRed";
@@ -135,7 +135,7 @@ function countCard(cardId) {
   }
   // update player score
   updateScore(cardValue);
-  if (turnCounter == 13) {
+  if (turnCounter === 13) {
       checkWinner();
   }
 }
